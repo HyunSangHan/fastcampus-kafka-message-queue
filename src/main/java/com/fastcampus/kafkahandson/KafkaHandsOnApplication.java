95f20b031c81a756efd383efac61b9980cc0b454
HyunSangHan <gustkd3@snu.ac.kr>
2023-11-30 23:21:00 +0900

    SpringBoot 기반의 프로젝트 시작

---
+++ src/main/java/com/fastcampus/kafkahandson/KafkaHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.kafkahandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaHandsOnApplication.class, args);
+	}
+
+}
