eff517ee74cebc0b415a0e260ea0e63f745253a0
HyunSangHan <gustkd3@snu.ac.kr>
2023-12-07 05:04:51 +0900

    JsonDeserializer 대신 StringDeserializer과 jackson 사용해서 deserailize 해보기

--- src/main/java/com/fastcampus/kafkahandson/config/ThirdKafkaConfig.java
+++ src/main/java/com/fastcampus/kafkahandson/config/ThirdKafkaConfig.java
@@ -1,8 +1,6 @@
 package com.fastcampus.kafkahandson.config;
 
-import com.fastcampus.kafkahandson.model.MyMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -22,26 +20,24 @@ public class ThirdKafkaConfig {
 
     @Bean
     @Qualifier("batchConsumerFactory")
-    public ConsumerFactory<String, MyMessage> batchConsumerFactory(KafkaProperties kafkaProperties) {
+    public ConsumerFactory<String, Object> batchConsumerFactory(@Qualifier("secondKafkaProperties") KafkaProperties secondKafkaProperties) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, secondKafkaProperties.getBootstrapServers());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, secondKafkaProperties.getConsumer().getKeyDeserializer());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, secondKafkaProperties.getConsumer().getValueDeserializer());
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, "false");
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, ConsumerConfig.DEFAULT_MAX_POLL_RECORDS);
-        return new DefaultKafkaConsumerFactory<>(
-            props,
-            new StringDeserializer(),
-            new JsonDeserializer<>(MyMessage.class)
-        );
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
     @Qualifier("batchKafkaListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, MyMessage> batchKafkaListenerContainerFactory(
-        @Qualifier("batchConsumerFactory") ConsumerFactory<String, MyMessage> batchConsumerFactory
+    public ConcurrentKafkaListenerContainerFactory<String, Object> batchKafkaListenerContainerFactory(
+        @Qualifier("batchConsumerFactory") ConsumerFactory<String, Object> batchConsumerFactory
     ) {
-        ConcurrentKafkaListenerContainerFactory<String, MyMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(batchConsumerFactory);
         factory.setBatchListener(true);
         factory.setConcurrency(1);
--- src/main/java/com/fastcampus/kafkahandson/consumer/MyThirdConsumer.java
+++ src/main/java/com/fastcampus/kafkahandson/consumer/MyThirdConsumer.java
@@ -1,6 +1,8 @@
 package com.fastcampus.kafkahandson.consumer;
 
 import com.fastcampus.kafkahandson.model.MyMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -17,10 +19,17 @@ public class MyThirdConsumer {
         groupId = "batch-test-consumer-group", // MyConsumer의 groupId와 반드시 달라야 함!
         containerFactory = "batchKafkaListenerContainerFactory"
     )
-    public void accept(List<ConsumerRecord<String, MyMessage>> messages) {
+    public void accept(List<ConsumerRecord<String, String>> messages) {
         System.out.println("[Third Consumer] Batch message arrived! - count " + messages.size());
+        ObjectMapper objectMapper = new ObjectMapper();
         messages.forEach(message -> {
-                System.out.println("ㄴ [Third Consumer] Value - " + message.value() + " / Offset - " + message.offset() + " / Partition - " + message.partition());
+            MyMessage myMessage = null;
+            try {
+                myMessage = objectMapper.readValue(message.value(), MyMessage.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("ㄴ [Third Consumer] Value - " + myMessage + " / Offset - " + message.offset() + " / Partition - " + message.partition());
             }
         );
     }
