6b7ddcbeaea081db08eb8a2a0c0bebb884f3aa30
HyunSangHan <gustkd3@snu.ac.kr>
2023-12-09 23:52:57 +0900

    기본적인 CRUD 기능을 할 컨트롤러 작성

--- src/main/java/com/fastcampus/kafkahandson/api/MyController.java
+++ src/main/java/com/fastcampus/kafkahandson/api/MyController.java
@@ -1,30 +1,79 @@
 package com.fastcampus.kafkahandson.api;
 
-import com.fastcampus.kafkahandson.model.MyMessage;
-import com.fastcampus.kafkahandson.producer.MyProducer;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fastcampus.kafkahandson.model.MyModel;
+import com.fastcampus.kafkahandson.service.MyService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class MyController {
 
-    private final MyProducer myProducer;
+    private final MyService myService;
 
-    @RequestMapping("/hello")
-    String hello() {
-        return "Hello World";
+    /**
+     * CRUD에서 HttpStatus를 위한 상세한 핸들링은 생략
+     */
+
+    @PostMapping("/greetings")
+    MyModel create(
+        @RequestBody Request request
+    ) {
+        if (
+            request == null ||
+            request.userId == null ||
+            request.userName == null ||
+            request.userAge == null ||
+            request.content == null
+        ) return null;
+
+        MyModel myModel = MyModel.create(
+            request.userId,
+            request.userAge,
+            request.userName,
+            request.content
+        );
+        return myService.save(myModel);
     }
 
-    @PostMapping("/message")
-    void message(
-            @RequestBody MyMessage message
+    @GetMapping("/greetings")
+    List<MyModel> list() {
+        return myService.findAll();
+    }
+
+    @GetMapping("/greetings/{id}")
+    MyModel get(
+        @PathVariable Integer id
     ) {
-        try {
-            myProducer.sendMessage(message);
-        } catch (JsonProcessingException e) {
-            e.fillInStackTrace();
-        }
+        return myService.findById(id);
+    }
+
+    @PatchMapping("/greetings/{id}")
+    MyModel update(
+        @PathVariable Integer id,
+        @RequestBody String content
+    ) {
+        if (id == null || content == null || content.isBlank()) return null;
+        MyModel myModel = myService.findById(id);
+        myModel.setContent(content);
+        return myService.save(myModel);
+    }
+
+    @DeleteMapping("/greetings/{id}")
+    void delete(
+        @PathVariable Integer id
+    ) {
+        myService.delete(id);
+    }
+
+    @Data
+    private static class Request {
+        Integer userId;
+        String userName;
+        Integer userAge;
+        String content;
     }
 }
