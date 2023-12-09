bbc7bc4663b5832d2d31b68b91085509e676c24b
HyunSangHan <gustkd3@snu.ac.kr>
2023-12-09 11:14:20 +0900

    배치리스너 설정 변경전으로 원복

--- src/main/java/com/fastcampus/kafkahandson/consumer/MyBatchConsumer.java
+++ src/main/java/com/fastcampus/kafkahandson/consumer/MyBatchConsumer.java
@@ -8,8 +8,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.fastcampus.kafkahandson.model.Topic.MY_JSON_TOPIC;
 
@@ -17,8 +15,6 @@ import static com.fastcampus.kafkahandson.model.Topic.MY_JSON_TOPIC;
 public class MyBatchConsumer {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     @KafkaListener(
         topics = { MY_JSON_TOPIC },
@@ -28,19 +24,14 @@ public class MyBatchConsumer {
     )
     public void listen(List<ConsumerRecord<String, String>> messages) {
         System.out.println("[Batch Consumer] Batch message arrived! - count " + messages.size());
-        messages.forEach(message -> executorService.submit(() -> {
+        messages.forEach(message -> {
             MyMessage myMessage;
             try {
                 myMessage = objectMapper.readValue(message.value(), MyMessage.class);
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
-            try {
-                Thread.sleep(1000);
-                System.out.println("ㄴ [Batch Consumer(" + Thread.currentThread().getId() + ")] " + "[Partition - " + message.partition() + " / Offset - " + message.offset() + "] " + myMessage);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }));
+            System.out.println("ㄴ [Batch Consumer(" + Thread.currentThread().getId() + ")] " + "[Partition - " + message.partition() + " / Offset - " + message.offset() + "] " + myMessage);
+        });
     }
 }
