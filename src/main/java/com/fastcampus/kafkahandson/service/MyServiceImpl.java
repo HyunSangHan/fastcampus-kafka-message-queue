a8dbc2c218c5efe71837afc648b9ac57a29c9858
HyunSangHan <gustkd3@snu.ac.kr>
2023-12-10 00:41:09 +0900

    Transactional annotation 활용

--- src/main/java/com/fastcampus/kafkahandson/service/MyServiceImpl.java
+++ src/main/java/com/fastcampus/kafkahandson/service/MyServiceImpl.java
@@ -4,8 +4,12 @@ import com.fastcampus.kafkahandson.data.MyEntity;
 import com.fastcampus.kafkahandson.data.MyJpaRepository;
 import com.fastcampus.kafkahandson.model.MyModel;
 import com.fastcampus.kafkahandson.model.MyModelConverter;
+import com.fastcampus.kafkahandson.model.OperationType;
+import com.fastcampus.kafkahandson.producer.MyCdcProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +19,7 @@ import java.util.Optional;
 public class MyServiceImpl implements MyService {
 
     private final MyJpaRepository myJpaRepository;
+    private final MyCdcProducer myCdcProducer;
 
     @Override
     public List<MyModel> findAll() {
@@ -29,13 +34,35 @@ public class MyServiceImpl implements MyService {
     }
 
     @Override
+    @Transactional
     public MyModel save(MyModel model) {
+        OperationType operationType = model.getId() == null ? OperationType.CREATE : OperationType.UPDATE;
         MyEntity entity = myJpaRepository.save(MyModelConverter.toEntity(model));
-        return MyModelConverter.toModel(entity);
+        MyModel resultModel = MyModelConverter.toModel(entity);
+        try {
+            myCdcProducer.sendMessage(
+                MyModelConverter.toMessage(
+                    resultModel.getId(),
+                    resultModel,
+                    operationType
+                )
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error processing JSON for sendMessage", e);
+        }
+        return resultModel;
     }
 
     @Override
+    @Transactional
     public void delete(Integer id) {
         myJpaRepository.deleteById(id);
+        try {
+            myCdcProducer.sendMessage(
+                MyModelConverter.toMessage(id, null, OperationType.DELETE)
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error processing JSON for sendMessage", e);
+        }
     }
 }
